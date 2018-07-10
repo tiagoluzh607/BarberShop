309931782f9598b73d1a73959ec529233e94d508
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-09 22:40:43 -0300

    Model - Criando Model Servico e Testando em uma classe Main

---
+++ src/View/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import Model.Servico;
+
+/**
+ *
+ * @author tiago
+ */
+public class Main {
+    
+    
+    public static void main(String[] args){
+      
+        String nome = "tiago";
+        System.out.println(nome);
+        
+        Servico servico = new Servico(1, "corte", 30);
+        
+        System.out.println(servico.getDescricao());
+        System.out.println(servico.getValor());
+        
+   }
+    
+    
+}
