0a3b5839178548b2629202ef2e1c6ce2d5d7c3d6
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-09 22:58:12 -0300

    Model - Criando Models Usuario e Cliente e Testando na Classe Main

--- src/View/Main.java
+++ src/View/Main.java
@@ -5,7 +5,9 @@
  */
 package View;
 
+import Model.Cliente;
 import Model.Servico;
+import Model.Usuario;
 
 /**
  *
@@ -23,6 +25,14 @@ public class Main {
         
         System.out.println(servico.getDescricao());
         System.out.println(servico.getValor());
+        
+        
+        Usuario tiago = new Usuario(1, "tiago", "123456", "administrador", 'M', "09/05/1996", "992408406", "tiagoluz.h6607@gmail.com", "95412547845");
+        System.out.println(tiago.getNome());
+        System.out.println(tiago.getDataNascimento()); //debugar aqui
+        
+        Cliente link = new Cliente(1, "link", 'M', "09/05/1996", "92408407", "zelda@zelda.com", "9254785", "Rua 345 palmeiras, NH", "9330000000");
+        
         
    }
     
