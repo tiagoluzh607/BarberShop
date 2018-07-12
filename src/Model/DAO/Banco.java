be3043c684b6a81ceea7d17bcbbbba76de187847
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-12 10:22:27 -0300

    Model - Dao - Ajuste na instancia dos clientes

--- src/Model/DAO/Banco.java
+++ src/Model/DAO/Banco.java
@@ -35,17 +35,17 @@ public class Banco {
         
         Usuario usuario1 = new Usuario(1, "barbeiro", 'M', "09/05/1996", "30212121", "barbeiro@barbershop.com", "521454123", "barbeiro", "administrador");
         Usuario usuario2 = new Usuario(2, "estagiario", 'M', "09/05/1996", "30212122", "estagiario@barbershop.com", "451244123", "estagiario", "funcionario");
-        
-        Cliente cliente1 = new Cliente(1, "Alan Figueiredo", 'M', "30212126", "alanbarber@gmail.com", "5142487", "Rua jaboti 35 Canudos NH", "30/01/1995", "93300012");
-        Cliente cliente2 = new Cliente(2, "Judite Oliveira", 'F', "30212127", "juditeoliveirapet@gmail.com", "5142487745", "Rua campo bom 78 Centro NH", "30/08/1994", "93300045");
-        Cliente cliente3 = new Cliente(3, "Paulo Ricado", 'M', "30212128", "pauloricardopet@gmail.com", "78451458", "Rua Tres Irmaos 30 Vila Nova - NH", "24/04/1997", "933007496");
-        Cliente cliente4 = new Cliente(4, "Neymar Santos", 'M', "30212130", "neymarsantospet@gmail.com", "2745487", "Rua Lombardi 40 Canudos NH", "20/08/1995", "93300748");
-        Cliente cliente5 = new Cliente(5, "Debruine Pipoqueiro", 'M', "30212131", "debruinepet@gmail.com", "4742487", "Rua pedro quaresma 784 Canudos Belgica", "13/09/1992", "933000847");
-        Cliente cliente6 = new Cliente(6, "Matuidi Pareira", 'M', "302122324", "matuidipet@gmail.com", "78512457", "Rua champions 10 Kephas Franca", "17/08/1994", "933000782");
-        Cliente cliente7 = new Cliente(7, "Marta de Souza", 'F', "302121292", "martasouzapet@gmail.com", "4658237314", "Rua tesla 215 Canudos NH", "25/03/1993", "93352012");
-        Cliente cliente8 = new Cliente(8, "Aline Ferrari", 'F', "302121451", "alineferraripet@gmail.com", "8475123687", "Rua hyrule 32 Canudos NH", "04/03/1990", "9334714747");
-        Cliente cliente9 = new Cliente(9, "Samuel Coltinho", 'M', "302121189", "samuelcoltinhopet@gmail.com", "74595142487", "Rua floresta 12 Canudos NH", "09/02/1998", "9330046364");
-        Cliente cliente10 = new Cliente(10, "Felipe Luis", 'M', "3021212478", "felipeluispet@gmail.com", "845713647", "Rua marechal 1023 Canudos NH", "12/06/1999", "933000874");
+         
+        Cliente cliente1 = new Cliente(1, "Alan Figueiredo", 'M', "30/01/1995", "30212126", "alanbarber@gmail.com", "5142487", "Rua jaboti 35 Canudos NH",  "93300012");
+        Cliente cliente2 = new Cliente(2, "Judite Oliveira", 'F', "30/08/1994", "30212127", "juditeoliveirapet@gmail.com", "5142487745", "Rua campo bom 78 Centro NH",  "93300045");
+        Cliente cliente3 = new Cliente(3, "Paulo Ricado", 'M', "24/04/1997", "30212128", "pauloricardopet@gmail.com", "78451458", "Rua Tres Irmaos 30 Vila Nova - NH",  "933007496");
+        Cliente cliente4 = new Cliente(4, "Neymar Santos", 'M', "20/08/1995", "30212130", "neymarsantospet@gmail.com", "2745487", "Rua Lombardi 40 Canudos NH",  "93300748");
+        Cliente cliente5 = new Cliente(5, "Debruine Pipoqueiro", 'M', "13/09/1992", "30212131", "debruinepet@gmail.com", "4742487", "Rua pedro quaresma 784 Canudos Belgica",  "933000847");
+        Cliente cliente6 = new Cliente(6, "Matuidi Pareira", 'M', "17/08/1994", "302122324", "matuidipet@gmail.com", "78512457", "Rua champions 10 Kephas Franca",  "933000782");
+        Cliente cliente7 = new Cliente(7, "Marta de Souza", 'F', "25/03/1993", "302121292", "martasouzapet@gmail.com", "4658237314", "Rua tesla 215 Canudos NH",  "93352012");
+        Cliente cliente8 = new Cliente(8, "Aline Ferrari", 'F', "04/03/1990", "302121451", "alineferraripet@gmail.com", "8475123687", "Rua hyrule 32 Canudos NH",  "9334714747");
+        Cliente cliente9 = new Cliente(9, "Samuel Coltinho", 'M', "09/02/1998", "302121189", "samuelcoltinhopet@gmail.com", "74595142487", "Rua floresta 12 Canudos NH",  "9330046364");
+        Cliente cliente10 = new Cliente(10, "Felipe Luis", 'M', "12/06/1999", "3021212478", "felipeluispet@gmail.com", "845713647", "Rua marechal 1023 Canudos NH",  "933000874");
         
         Servico servico1 = new Servico(1, "Corte Simples", 18);
         Servico servico2 = new Servico(2, "Corte Degrade", 30);
