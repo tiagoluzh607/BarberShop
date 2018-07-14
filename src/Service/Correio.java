e1d886bdba442c52eef9cb4692a6219473e75568
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-13 21:57:50 -0300

    Service - Correio - Melhorando Layout do Email

--- src/Service/Correio.java
+++ src/Service/Correio.java
@@ -36,8 +36,8 @@ public class Correio {
         String horaAgendamento = agendamento.getHoraFormatada();
         float valor = agendamento.getValor();
         
-        return "Olá "+ nomeCliente + " Vai dar um tapa no visu é... Seu agendamento para"+ servico+", esta marcado para dia "+ dataAgendamento
-                +" às "+ horaAgendamento + " O preço para você sai baratin fica R$"+ valor + "Grande Abraço!!";
+        return "Olá "+ nomeCliente + " Vai dar um tapa no visu é... Seu agendamento para "+ servico+", esta marcado para dia "+ dataAgendamento
+                +" às "+ horaAgendamento + "\n\n O preço para você sai baratin fica R$"+ valor + " \n\nGrande Abraço!!";
     }
     
 }
